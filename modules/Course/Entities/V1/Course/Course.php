09c13c1bb0c9dece77d32ffe2e1080cf528bc55e
RealMrHex <RealMrHex@gmail.com>
2023-12-12 06:56:19 +0330

    Add factory to the venus command's stub

--- modules/Course/Entities/V1/Course/Course.php
+++ modules/Course/Entities/V1/Course/Course.php
@@ -2,8 +2,10 @@
 
 namespace Modules\Course\Entities\V1\Course;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Base\Entities\V1\BaseModel;
+use Modules\Course\Database\Factories\V1\CourseFactory\CourseFactory;
 
 class Course extends BaseModel
 {
@@ -29,4 +31,17 @@ class Course extends BaseModel
     protected $casts = [
         #CourseFields::CREATED_AT => 'datetime',
     ];
+
+    /**
+     * Get a new factory instance for the model.
+     *
+     * @param callable|array|int|null $count
+     * @param callable|array          $state
+     *
+     * @return Factory<static>
+     */
+    public static function factory($count = null, $state = []): Factory
+    {
+        return CourseFactory::new();
+    }
 }
