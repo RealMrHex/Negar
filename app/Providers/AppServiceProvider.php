e4826243ce9cbb753a01d978789982e33bcd1a3b
RealMrHex <RealMrHex@gmail.com>
2024-01-06 16:22:09 +0330

    force url from app service provider [not-sure-about-ot]

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,23 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider { }
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application service
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        URL::forceRootUrl(config('app.url'));
+        if (str_contains(config('app.url'), 'https://'))
+        {
+            URL::forceScheme('https');
+            request()->server->set('HTTPS', request()->header('X-Forwarded-Proto', 'https') == 'https' ? 'on' : 'off');
+        }
+    }
+}
